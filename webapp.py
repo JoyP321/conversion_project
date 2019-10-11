89313040cffdcf5c0a5b565d5c7f62988a2e5936
JoyP321 <54326509+JoyP321@users.noreply.github.com>
2019-10-11 09:36:13 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,6 +8,12 @@ def render_main():
 
 @app.route("/p1") #annotations tell which function go with which request
 def render_page1():
+    meters = request.args['input']
+    unit = request.args['newUnit']
+    if unit == "centimeters" :
+        toReturn = meters*100.0
+    if unit == "inches" :
+        toReturn = meters*39.3701
     return render_template('page1.html')
 
 @app.route("/p2")
