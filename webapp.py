31e05b31bfe0eb1dbd2d3fe3cd2ade3e675e62fe
JoyP321 <54326509+JoyP321@users.noreply.github.com>
2019-10-15 09:38:11 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,17 +8,7 @@ def render_main():
 
 @app.route("/p1") #annotations tell which function go with which request
 def render_page1():
-    toReturn = -1
-    if request.args['input'] != null and request.args['newUnit'] != null:
-        meters = float(request.args['input'])
-        unit = request.args['newUnit']
-        if unit == "centimeters" :
-            toReturn = meters*100.0
-        if unit == "inches" :
-            toReturn = meters*39.3701
-        if unit == "feet" :
-            toReturn = meters*3.28084
-    return render_template('page1.html', responseFromServer =  toReturn)
+    return render_template('page1.html')
 
 @app.route("/response")
 def render_response():
